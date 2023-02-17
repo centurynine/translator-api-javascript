1618a6c66712e170c7ef447bae1e6600d5ef9fe3
CenturyNine <lowlifeix@gmail.com>
2023-02-17 22:52:00 +0700

    js update

---
+++ function.js
@@ -0,0 +1,6 @@
+const translateButton = document.querySelector('.translate-button');
+const inputText = document.querySelector('.from-text');
+const outputText = document.querySelector('.to-text');
+const selectTag = document.querySelectorAll('select');
+const icons = document.querySelector('.row i');
+const exchangeIcon = document.querySelector('.exchange');
